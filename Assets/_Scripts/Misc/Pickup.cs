d0587e6e934d0e222b22a85dc3fca2121b7c1bd6
kamka1 <151570680+kamka1@users.noreply.github.com>
2024-02-08 01:48:30 +0100

    UI & drops
    Health ui implemented
    stamina and coins graphic only
    bush and enemy drop coin/health recovery/stammina recovery

---
+++ Assets/_Scripts/Misc/Pickup.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    private enum PickUpType
+    {
+        GoldCoin,
+        StaminaGlobe,
+        HealthGlobe,
+    }
+    [SerializeField] private PickUpType pickUpType;
+    [SerializeField] private float pickUpDistance = 5f;
+    [SerializeField] private float accelartionRate = .2f;
+    [SerializeField] private float moveSpeed = 3f;
+    private Vector3 moveDir;
+    private Rigidbody2D rb;
+
+    private void Awake(){
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update(){
+        Vector3 playerPos = PlayerController.Instance.transform.position;
+        if(Vector3.Distance(transform.position, playerPos) < pickUpDistance){
+            moveDir = (playerPos - transform.position).normalized;
+            moveSpeed += accelartionRate;
+        } else {
+            moveDir = Vector3.zero;
+            moveSpeed = 0;
+        }
+    }
+    private void FixedUpdate(){
+        rb.velocity = moveDir * moveSpeed * Time.deltaTime;
+    }
+  private void OnTriggerStay2D(Collider2D other){
+    if(other.gameObject.GetComponent<PlayerController>()){
+        DetectPickupType();
+        Destroy(gameObject);
+    }
+  }
+  private void DetectPickupType(){
+    switch(pickUpType)
+    {
+        case PickUpType.GoldCoin:
+        //dsada
+        Debug.Log("Gold");
+        break;
+        case PickUpType.HealthGlobe:
+        PlayerHealth.Instance.HealPlayer();
+        Debug.Log("Helth");
+        break;
+        case PickUpType.StaminaGlobe:
+        Debug.Log("Stamina ");
+        //sdasda
+        break;
+    }
+  }
+}
--- Assets/_Scripts/Player/PlayerHealth.cs
+++ Assets/_Scripts/Player/PlayerHealth.cs
@@ -1,46 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PlayerHealth : MonoBehaviour
+public class PlayerHealth : Singleton<PlayerHealth>
 {
     [SerializeField] private int maxHealth = 3;
     [SerializeField] private float knockBackThrustAmount = 10f;
     [SerializeField] private float damageRecoveryTime = 1f;
+    private Slider healthSlider;
     private int currentHealth;
     private bool canTakeDamage = true;
 
     private Knockback knockback;
     private Flash flash;
 
-    private void Awake(){
+    protected override void Awake(){
+        base.Awake();
         flash = GetComponent<Flash>();
         knockback = GetComponent<Knockback>();
     }
 
     private void Start(){
         currentHealth = maxHealth;
+        UpdateHealthSlider();
     }
-
+        public void HealPlayer(){
+        if(currentHealth < maxHealth){
+        currentHealth +=1;
+        UpdateHealthSlider();
+        }
+    }
     private void OnCollisionStay2D(Collision2D other) {
         EnemyAi enemy = other.gameObject.GetComponent<EnemyAi>();
 
-        if (enemy && canTakeDamage) {
-            TakeDamage(1);
-            knockback.GetKnockedBack(other.gameObject.transform, knockBackThrustAmount);
-            StartCoroutine(flash.FlashRoutine());
+        if (enemy) {
+            TakeDamage(1, other.transform);
         }
     }
 
-    private void TakeDamage(int damageAmount) {
+    private void TakeDamage(int damageAmount, Transform hitTransform) {
+        if (!canTakeDamage) { return; }
+
+        knockback.GetKnockedBack(hitTransform, knockBackThrustAmount);
+        StartCoroutine(flash.FlashRoutine());
         canTakeDamage = false;
         currentHealth -= damageAmount;
         StartCoroutine(DamageRecoveryRoutine());
+        UpdateHealthSlider();
+        CheckIfPlayerDeath();
     }
 
+        private void CheckIfPlayerDeath() {
+        if (currentHealth <= 0) {
+            currentHealth = 0;
+            Debug.Log("Player Death");
+        }
+    }
     private IEnumerator DamageRecoveryRoutine() {
         yield return new WaitForSeconds(damageRecoveryTime);
         canTakeDamage = true;
+    }
+
+    private void UpdateHealthSlider(){
+        if(healthSlider ==null){
+            healthSlider = GameObject.Find("Health Slider").GetComponent<Slider>();
+        }
+        healthSlider.maxValue = maxHealth;
+        healthSlider.value = currentHealth;
     }
 
 }
